6809092884db77dcaf6683e6a03240e4980ffcd6
Ross Addison <rossaddison@users.noreply.github.com>
2026-02-07 15:10:57 +0000

    BootstrapCdnAsset 5.0.0-beta1 to 5.3.8

--- src/Assets/BootstrapCdnAsset.php
+++ src/Assets/BootstrapCdnAsset.php
@@ -22,8 +22,8 @@ final class BootstrapCdnAsset extends AssetBundle
      */
     public array $css = [
         [
-            'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css',
-            'integrity' => 'sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1',
+            'https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css',
+            'integrity' => 'sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB',
             'crossorigin' => 'anonymous',
         ],
     ];
@@ -33,8 +33,8 @@ final class BootstrapCdnAsset extends AssetBundle
      */
     public array $js = [
         [
-            'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js',
-            'integrity' => 'sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW',
+            'https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js',
+            'integrity' => 'sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI',
             'crossorigin' => 'anonymous',
         ],
     ];
